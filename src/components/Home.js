d7b500d22ed6b532b530611c0da9ee9a96f9958c
smartbison <121305911+smartbison@users.noreply.github.com>
2024-12-29 19:45:24 -0500

    fixed a lot of minor design issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,27 +27,6 @@ function Home({ isLoggedIn, userData }) {
   const navigate = useNavigate();
   const [teamIcon, setTeamIcon] = useState(null);
 
-  useEffect(() => {
-    if (userData && userData.team) {
-      switch (userData.team) {
-        case 'NobleFranklinRiders':
-          setTeamIcon(NobleFranklinRiders);
-          break;
-        case 'BlazingRamses':
-          setTeamIcon(BlazingRamses);
-          break;
-        case 'TarHeelTrojans':
-          setTeamIcon(TarHeelTrojans);
-          break;
-        case 'SixerJ0rdans':
-          setTeamIcon(SixerJ0rdans);
-          break;
-        default:
-          break;
-      }
-    }
-  }, [userData]);
-
   const handleTeamsClick = (team) => {
     navigate(`/bcl/teams#${team}`);
   };
@@ -83,7 +62,7 @@ function Home({ isLoggedIn, userData }) {
   return (
     <main>
       <div className="home-container">
-        <h2>Welcome to BOUNC!</h2>
+        <h2 className="welcome">Welcome to BOUNC!</h2>
         {isLoggedIn && userData ? (
           <div className="content">
             <div className="team-info">
@@ -100,7 +79,7 @@ function Home({ isLoggedIn, userData }) {
           </div>
         ) : (
           <div 
-            className="background-image" 
+            className="team-icon-container panel" 
             style={{ backgroundImage: `url(${background})` }}
           >
             <img 
@@ -133,22 +112,17 @@ function Home({ isLoggedIn, userData }) {
               className="icon sixer-jordans" 
               onClick={() => handleTeamsClick('T4B')} 
             />
-            <img 
-              src={sign} 
-              alt="Sign" 
-              className="icon sign" 
-              onClick={handleSignClick} 
-            />
+                          <img 
+        src={sign} 
+        alt="Sign" 
+        className="icon" id="sign" 
+        onClick={handleSignClick} 
+      />
           </div>
         )}
 
         {/* Heel Life image with icon */}
-        <div className="heel-life-container" onClick={handleHeelLifeClick}>
-          <img 
-            src={heellifepagebackground} 
-            alt="Heel Life Page Background" 
-            className="heel-life-image" 
-          />
+        <div className="panel" id = "heel-life" onClick={handleHeelLifeClick} style={{"background-image":`url(${heellifepagebackground})`}}>
           <img 
             src={heellifeIcon} 
             alt="Heel Life Icon" 
@@ -157,12 +131,7 @@ function Home({ isLoggedIn, userData }) {
         </div>
 
         {/* New Cricket Background Image with Icons */}
-        <div className="cricket-background-container">
-          <img 
-            src={cricketbackground} 
-            alt="Cricket Background" 
-            className="cricket-background-image" 
-          />
+        <div className=" panel" style={{"background-image":`url(${cricketbackground})`}} >
           <div className="cricket-icons">
             <img 
               src={lIcon} 
@@ -191,13 +160,7 @@ function Home({ isLoggedIn, userData }) {
           </div>
         </div>
 
-        {/* New Social Media Background Image with Icons */}
-        <div className="socials-background-container">
-          <img 
-            src={socials} 
-            alt="Social Media Background" 
-            className="socials-background-image" 
-          />
+        <div className="socials-background-container panel" style = {{"background-image":`url(${socials})`}}>
           <div className="social-icons">
             <img 
               src={tiktokIcon} 
